e20f5f260f73d5c6a4e21c58817243d68962cfe0
wiidede <wiixdede@gmail.com>
2025-03-06 15:54:04 +0800

    chore: init db

--- server/database/migrations/0000_serious_shinobi_shaw.sql
+++ server/database/migrations/0000_serious_shinobi_shaw.sql
@@ -24,9 +24,26 @@ CREATE TABLE `photos` (
 	`latitude` real,
 	`longitude` real,
 	`taken_at` integer,
-	`aspect_ratio` real DEFAULT 1.5,
+	`aspect_ratio` real,
 	`priority_order` real,
 	`hidden` integer DEFAULT false,
 	`updated_at` integer DEFAULT CURRENT_TIMESTAMP,
 	`created_at` integer DEFAULT CURRENT_TIMESTAMP
 );
+--> statement-breakpoint
+CREATE TABLE `photo_tags` (
+	`photo_id` text NOT NULL,
+	`tag_id` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(`photo_id`, `tag_id`),
+	FOREIGN KEY (`photo_id`) REFERENCES `photos`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`tag_id`) REFERENCES `tags`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE TABLE `tags` (
+	`id` text PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`created_at` integer DEFAULT CURRENT_TIMESTAMP
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `tags_name_unique` ON `tags` (`name`);
